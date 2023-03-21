c826df249988b08a659496a8d5c5ef8aaefd1b3d
o  | |  o <90270276+Agugodzic@users.noreply.github.com>
2023-03-21 15:51:07 -0300

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const DB = db;
 app.use(express.json({limit:'50mb'}));
 app.use(morgan('dev'));
 app.use(cors({
-  origin:"https://pupitapetshop.com.ar"
+  origin:"http://localhost:4200"
 }));
 
 (async ()=> {
