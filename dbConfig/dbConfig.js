703b6c1b6026c5f8fdc22b35d254247b923d625e
o  | |  o <90270276+Agugodzic@users.noreply.github.com>
2023-03-25 13:20:35 -0300

    Update dbConfig.js

--- dbConfig/dbConfig.js
+++ dbConfig/dbConfig.js
@@ -6,6 +6,7 @@ import mysql2 from 'mysql2';
 const config = process.env;
 
 const db = new Sequelize(config.DATABASE_NAME, config.DB_USER_NAME , config.DB_PASSWORD, {
+
   host: config.DB_HOST,
   //port: '3306',
   password: config.DB_PASSWORD,
@@ -13,10 +14,11 @@ const db = new Sequelize(config.DATABASE_NAME, config.DB_USER_NAME , config.DB_P
   dialectModule: mysql2,
 
   pool: {
-    max: 5,
+    max: 6,
     min: 0,
     idle: 10000
   }
+
 });
 
 export default db;
