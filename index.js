1c16ef1e49b6155290a369bd22e1765d050fe018
o  | |  o <90270276+Agugodzic@users.noreply.github.com>
2023-03-25 16:41:29 -0300

    cors pupitapetshop

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const DB = db;
 app.use(express.json({limit:'50mb'}));
 app.use(morgan('dev'));
 app.use(cors({
-  origin:"http://localhost:4200"
+  origin:"https://pupitapetshop.com.ar"
 }));
 
 (async ()=> {
@@ -31,7 +31,7 @@ app.use(cors({
 })();
 
 app.get("/",(req,res)=>{
-  res.send("Pupita Backend config cors")
+  res.send("Pupita Backend config cors pps")
 });
 
 app.use(router);
