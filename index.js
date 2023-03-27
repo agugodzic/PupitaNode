11e206f64296d80c2d2660231586a3521d4799e2
o  | |  o <90270276+Agugodzic@users.noreply.github.com>
2023-03-27 16:55:27 -0300

    vercel

--- index.js
+++ index.js
@@ -15,21 +15,10 @@ const DB = db;
 app.use(express.json({limit:'50mb'}));
 app.use(morgan('dev'));
 
-var whitelist = ['http://localhost:4200', 'https://pupitapetshop.com.ar']
-
-var corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
-  }
-}
-
 app.use(cors({
-  corsOptions
+  origin:"https://pupitapetshop.com.ar"
 }));
+/*
 
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
@@ -38,8 +27,7 @@ app.use((req, res, next) => {
     res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
     next();
 });
-
-
+*/
 
 (async ()=> {
   try{
