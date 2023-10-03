6fc5b419c73220a82117bd9786e227243a7ffb7e
o  | |  o <90270276+Agugodzic@users.noreply.github.com>
2023-10-03 19:50:42 -0300

    Update productRoutes.js

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import Sequelize, { DatabaseError, Op } from "sequelize";
+import Sequelize, {Op } from "sequelize";
 import Product from "../models/product.js";
 import previewGenerate from "../controller/productController.js";
 import sharp from 'sharp';
